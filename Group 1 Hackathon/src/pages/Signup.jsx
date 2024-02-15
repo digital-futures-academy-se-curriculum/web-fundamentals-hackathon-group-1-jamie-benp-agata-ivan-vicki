f31004123f38ebe276ed7745f1027571153fd65c
jaymify <jarnelldesign@gmail.com>
2024-02-15 17:39:56 +0000

    Co-authored-by: Vicki <VSeaman8@users.noreply.github.com> Co-authored-by: MagicPolygon <MagicPolygon@users.noreply.github.com> Co-authored-by: BenParkes2000 <BenParkes2000@users.noreply.github.com>

--- Group 1 Hackathon/src/pages/Signup.jsx
+++ Group 1 Hackathon/src/pages/Signup.jsx
@@ -7,29 +7,51 @@ function Signup() {
                 <div className="row">
                     <div className="col-md-6">
                         {/* Left Column Content */}
-                        <h1>WHATS ON MY FRIENDS</h1>
+                        <img
+                            className="deadpool"
+                            src="https://static1.srcdn.com/wordpress/wp-content/uploads/2017/12/Deadpool-2-Cable-banner.jpg"
+                            alt="Block 01 Image"
+                        />
+                        <br></br>
+                        <br></br>
+                        <img
+                            className="deadpool"
+                            src="https://static1.srcdn.com/wordpress/wp-content/uploads/2017/12/Deadpool-2-Cable-banner.jpg"
+                            alt="Block 01 Image"
+                        />
+                        <br></br>
+                        <br></br>
+                        <img
+                            className="deadpool"
+                            src="https://static1.srcdn.com/wordpress/wp-content/uploads/2017/12/Deadpool-2-Cable-banner.jpg"
+                            alt="Block 01 Image"
+                        />
                     </div>
-                    <div className="col-md-6">
-                        {/* Right Column Content */}
-                        <form action="/submit-form" method="POST">
-                            <label htmlFor="title">Title:</label><br />
-                            <input type="text" id="title" name="title" required /><br /><br />
-                            <label htmlFor="firstName">First Name:</label><br />
-                            <input type="text" id="firstName" name="firstName" required /><br /><br />
-                            <label htmlFor="lastName">Last Name:</label><br />
-                            <input type="text" id="lastName" name="lastName" required /><br /><br />
-                            <label htmlFor="email">Email:</label><br />
-                            <input type="email" id="email" name="email" required /><br /><br />
-                            <label htmlFor="dateOfBirth">Date of Birth:</label><br />
-                            <input type="date" id="dateOfBirth" name="dateOfBirth" required /><br /><br />
-                            <label htmlFor="phone">Phone:</label><br />
-                            <input type="tel" id="phone" name="phone" required /><br /><br />
-                            <input type="submit" value="Submit" />
-                        </form>
+                    <div className="col-md-3">
+                        <div className="card" id="formCard">
+                            <div class="card-body">
+
+                                {/* Right Column Content */}
+                                <form action="/submit-form" method="POST">
+                                    <label htmlFor="title">Title*:</label><br />
+                                    <input type="text" id="title" name="title" placeholder="Mr/Mrs/Miss/Other" required /><br /><br />
+                                    <label htmlFor="firstName">First Name*:</label><br />
+                                    <input type="text" id="firstName" name="firstName" placeholder="Your First Name" required /><br /><br />
+                                    <label htmlFor="lastName">Last Name*:</label><br />
+                                    <input type="text" id="lastName" name="lastName" placeholder="Your Last Name" required /><br /><br />
+                                    <label htmlFor="email">Email*:</label><br />
+                                    <input type="email" id="email" name="email" placeholder="email@email.com" required /><br /><br />
+                                    <label htmlFor="dateOfBirth">Date of Birth:</label><br />
+                                    <input type="date" id="dateOfBirth" name="dateOfBirth" required /><br /><br />
+                                    <label htmlFor="phone">Phone*:</label><br />
+                                    <input type="tel" id="phone" name="phone" placeholder="07999999999" required /><br /><br />
+                                    <input type="submit" value="Sign Me Up" style={{ backgroundColor: '#f2c338', color: 'black' }} />
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </>
     );
 }
