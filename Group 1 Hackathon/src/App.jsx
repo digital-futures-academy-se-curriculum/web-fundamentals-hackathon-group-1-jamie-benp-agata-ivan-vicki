212565337aee92a321bff045aeb60edafb601a2c
BenParkes2000 <ba.parkes@virginmedia.com>
2024-02-15 12:05:33 +0000

    Home Page
    Added home page - created via LiveShare, (further CSS may be needed)

--- Group 1 Hackathon/src/App.jsx
+++ Group 1 Hackathon/src/App.jsx
@@ -1,39 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import SocialIcons from "./SocialIcons";
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./Navbar";
+import Block01 from "./Block01";
+import Block02 from "./Block02";
+import Block03 from "./Block03";
+import SocialIcons from "./SocialIcons";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
       <div className="App">
-        {/* Other app content */}
+        <Navbar />
+        <Block01 />
+        <Block02 />
+        <Block03 />
         <SocialIcons />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="copyRight"> © DF Cinemas - 2021 -</p>
     </>
   );
 }
--- Group 1 Hackathon/src/SocialIcons.jsx
+++ Group 1 Hackathon/src/SocialIcons.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 const SocialIcons = () => {
   return (
     <div className="social-icons">
-      <a href="https://www.facebook.com/" target="_blank">
+      <a href="https://www.facebook.com/digital.futures2021/" target="_blank">
         <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
       <a href="https://twitter.com/" target="_blank">
